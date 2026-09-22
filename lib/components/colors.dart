56e807ca05c965ffe0d80c3c270074d567396f6a
juzuk1q <artemtselikov2011@gmail.com>
2026-09-22 12:23:53 +0500

    Добавлен класс цветов.

---
+++ lib/components/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+// цвета для различных элементов. (не буду дальше комментить, мне лень)
+class AppColors{
+  const AppColors._();
+
+  static const primary = Color(0xff004AC6);
+  static const secondary = Color(0xff505F76);
+  static const tertiary = Color(0xff943700);
+  static const error = Color(0xffBA1A1A);
+  static const white = Color(0xffFFFFFF);
+  static const black = Color(0xff191B23);
+  static const grey = Color(0xffC3C6D7);
+  static const darkenWhite = Color(0xffFAF8FF);
+}
--- lib/log.dart
+++ lib/log.dart
@@ -4,14 +4,14 @@ import 'package:logger/logger.dart';
 // Уровни логирования
 enum LogLevel { info, debug, error }
 
-//
 final appLogger = Logger(printer: SimplePrinter(colors: false, printTime: false), level: kDebugMode ? Level.debug : Level.nothing);
 
 // Метод отображения лога
 // Входящие данные: тэг, событие, детали и уровень лога
 // Возвращаемые значения: форматирование лога
-// Автор создания: 5
-// Дата создания: 19.05.2026
+// Автор создания: 3
+// Дата создания: 22.09.2026
+
 void appLog(String tag, String event, String details, {LogLevel level = LogLevel.info}) {
   final mes = '[$tag]: $event - $details';
   switch (level) {
