e83a0c2d552d512b872b6eca20d6b064a90db2b6
juzuk1q <artemtselikov2011@gmail.com>
2026-09-22 19:59:50 +0500

    feat(font): Edited some params

--- lib/components/fonts.dart
+++ lib/components/fonts.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vize/vize.dart' hide TextStyle;
+import 'package:vize/vize.dart';
 import 'package:ui_kit/components/colors.dart';
 
 // класс текста,
@@ -11,17 +11,20 @@ class AppText{
     required FontWeight fontWeight,
     Color? color,
     double? letterSpacing,
+    required double height,
   }) => TextStyle(
     fontFamily: 'Manrope',
     fontSize: fontSize,
     fontWeight: fontWeight,
     color: color ?? AppColors.black,
     letterSpacing: letterSpacing ?? 0,
+    height: height / fontSize,
+    package: 'ui_kit'
   );
 
-  static TextStyle get header => base(fontSize: 24.fh, fontWeight: .w700, letterSpacing: -0.48);
-  static TextStyle get subHeader => base(fontSize: 18.fh, fontWeight: .w600, letterSpacing: -0.18);
-  static TextStyle get bodyM => base(fontSize: 16.fh, fontWeight: .w400);
-  static TextStyle get bodyS => base(fontSize: 14.fh, fontWeight: .w400);
-  static TextStyle get fieldLabel => base(fontSize: 14.fh, fontWeight: .w600);
+  static TextStyle get header => base(fontSize: 24.fh, fontWeight: .w700, letterSpacing: -0.48, height: 32);
+  static TextStyle get subHeader => base(fontSize: 18.fh, fontWeight: .w600, letterSpacing: -0.18, height: 24);
+  static TextStyle get bodyM => base(fontSize: 16.fh, fontWeight: .w400, height: 24);
+  static TextStyle get bodyS => base(fontSize: 14.fh, fontWeight: .w400, height: 20);
+  static TextStyle get fieldLabel => base(fontSize: 14.fh, fontWeight: .w600, height: 20);
 }
