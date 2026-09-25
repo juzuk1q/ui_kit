3843a3e6ce6b9a65689d5cd5ca39e996cb17c164
juzuk1q <artemtselikov2011@gmail.com>
2026-09-25 13:40:29 +0500

    Added new UI-elements

--- lib/components/avatar.dart
+++ lib/components/avatar.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:vize/vize.dart';
 import 'package:ui_kit/ui_kit.dart';
 
+enum AvatarType {
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  initials,
+}
+
 class CustomAvatars extends StatelessWidget {
   final double size;
+  final AvatarType? avatar;
   final ImageProvider? image;
   final String initials;
   final String? wth; // под аватаркой
@@ -11,20 +21,42 @@ class CustomAvatars extends StatelessWidget {
   const CustomAvatars({
     super.key,
     this.size = 64,
+    this.avatar,
     this.image,
     required this.initials,
     this.wth = null,
   });
 
+  String? get _assetPath => switch (avatar) {
+    AvatarType.avatar1 => '/assets/images/avatar1.png',
+    AvatarType.avatar2 => '/assets/images/avatar2.png',
+    AvatarType.avatar3 => '/assets/images/avatar3.png',
+    AvatarType.avatar4 => '/assets/images/avatar4.png',
+    AvatarType.avatar5 => '/assets/images/avatar5.jpg',
+    AvatarType.initials => null,
+    null => null,
+  };
+
+  ImageProvider? get _Image2 {
+    if (image != null) return image;
+    final path = _assetPath;
+    if (path != null) {
+      return AssetImage(path, package: 'ui_kit');
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final img = _Image2;
+
     final icon = Container(
       height: size.fh,
       width: size.fw,
       decoration: BoxDecoration(
         shape: .circle,
         color: AppColors.darkenWhite,
-        border: .all(color: image != null ? AppColors.primary : AppColors.grey),
+        border: .all(color: img != null ? AppColors.primary : AppColors.grey),
       ),
       child: ClipOval(
         child: Stack(
@@ -41,10 +73,12 @@ class CustomAvatars extends StatelessWidget {
               ),
             ),
             if (image != null)
-              Image(
-                image: image!,
-                fit: .cover,
-                errorBuilder: (_, __, ___) => SizedBox.shrink(),
+              Positioned.fill(
+                child: Image(
+                  image: img!,
+                  fit: .cover,
+                  errorBuilder: (_, __, ___) => SizedBox.shrink(),
+                ),
               ),
           ],
         ),
@@ -58,7 +92,7 @@ class CustomAvatars extends StatelessWidget {
         if (wth != null) ...[
           SizedBox(height: 8.fh),
           Text(wth!, style: AppText.bodyM)
-        ]
+        ],
       ],
     );
   }
